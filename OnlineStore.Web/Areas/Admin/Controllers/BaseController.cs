2709cadd00e89ab2e0c8922e00f29de7e0d4c8f9
quanhieu <tqhieu91@gmail.com>
2017-05-10 11:59:33 +0700

    Update Template

---
+++ OnlineStore.Web/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineStore.Common;
+using OnlineStore.Data.Entities;
+
+namespace OnlineStore.Web.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+            if (Session[CommonConstants.USER_SESSION] == null)
+            {
+                Response.Redirect("~/Admin/Account/Login");
+            }
+        }
+
+        public string GetUserId()
+        {
+            if (Session[CommonConstants.USER_SESSION] != null)
+            {
+                var user = Session[CommonConstants.USER_SESSION] as User;
+                return user.Id;
+            }
+            return string.Empty;
+        }
+    }
+}

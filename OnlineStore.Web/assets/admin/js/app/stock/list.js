cd155f710385c992230a32c66f04742f23fddb6f
quanhieu <hieu.truongquan@gmail.om>
2017-05-19 00:22:26 +0700

    Update code

---
+++ OnlineStore.Web/assets/admin/js/app/stock/list.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function () {
+
+    LoadSorting();
+
+    $("th.sortable").click(function (evt) {
+        var sortfield = $(evt.target).data("sortfield");
+        if ($("#hdSortField").val() === sortfield) {
+            if ($("#hdSortDirection").val() === "ascending") {
+                $("#hdSortDirection").val("descending");
+            }
+            else {
+                $("#hdSortDirection").val("ascending");
+                $(this).parent().removeClass("sorting");
+            }
+        }
+        else {
+            $("#hdSortField").val(sortfield);
+            $("#hdSortDirection").val("ascending");
+        }
+        fnSearch();
+    });
+});
+
+function LoadSorting() {
+    var sortField = $("#hdSortField").val();
+    var sortDirection = $("#hdSortDirection").val();
+
+    $("th.sortable").each(function () {
+        if ($(this).data("sortfield") === sortField) {
+            if (sortDirection === "ascending") {
+                $(this).removeClass("sorting");
+                $(this).addClass("sorting_desc");
+            }
+            else {
+                $(this).removeClass("sorting");
+                $(this).addClass("sorting_asc");
+            }
+        } else {
+            if ($(this).hasClass("sorting_desc")) {
+                $(this).removeClass("sorting_desc");
+                $(this).addClass("sorting");
+            }
+            else if ($(this).hasClass("sorting_asc")) {
+                $(this).removeClass("sorting_asc");
+                $(this).addClass("sorting");
+            }
+        }
+    });
+}

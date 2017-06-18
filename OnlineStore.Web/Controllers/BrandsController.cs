468a9342525de4d71528d79892e5437f25714592
Truong Quan Hieu <hieu.truongquan@gmail.com>
2017-06-18 15:52:49 +0700

    Update code

---
+++ OnlineStore.Web/Controllers/BrandsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnlineStore.Data.Entities;
+using OnlineStore.Data.Infrastructure;
+
+namespace OnlineStore.Web.Controllers
+{
+    public class BrandsController : Controller
+    {
+        private OnlineStoreDbContext db = new OnlineStoreDbContext();
+
+        // GET: Brands
+        public ActionResult Index()
+        {
+            var brands = db.Brands.Include(b => b.CreatedBy).Include(b => b.ModifiedBy);
+            return View(brands.ToList());
+        }
+
+        // GET: Brands/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // GET: Brands/Create
+        public ActionResult Create()
+        {
+            ViewBag.CreatedById = new SelectList(db.Users, "Id", "Username");
+            ViewBag.ModifiedById = new SelectList(db.Users, "Id", "Username");
+            return View();
+        }
+
+        // POST: Brands/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,CreatedOn,CreatedById,ModifiedOn,ModifiedById")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Brands.Add(brand);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CreatedById = new SelectList(db.Users, "Id", "Username", brand.CreatedById);
+            ViewBag.ModifiedById = new SelectList(db.Users, "Id", "Username", brand.ModifiedById);
+            return View(brand);
+        }
+
+        // GET: Brands/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CreatedById = new SelectList(db.Users, "Id", "Username", brand.CreatedById);
+            ViewBag.ModifiedById = new SelectList(db.Users, "Id", "Username", brand.ModifiedById);
+            return View(brand);
+        }
+
+        // POST: Brands/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,CreatedOn,CreatedById,ModifiedOn,ModifiedById")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(brand).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CreatedById = new SelectList(db.Users, "Id", "Username", brand.CreatedById);
+            ViewBag.ModifiedById = new SelectList(db.Users, "Id", "Username", brand.ModifiedById);
+            return View(brand);
+        }
+
+        // GET: Brands/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: Brands/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Brand brand = db.Brands.Find(id);
+            db.Brands.Remove(brand);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
--- OnlineStore.Web/Controllers/HomeController.cs
+++ OnlineStore.Web/Controllers/HomeController.cs
@@ -26,5 +26,67 @@ namespace OnlineStore.Web.Controllers
 
             return View();
         }
+
+        // -- PARTIAL VIEW --
+
+        // Featured Product
+        public ActionResult _FeaturedProduct()
+        {
+            return PartialView();
+        }
+
+        // New Arrival Product
+        public ActionResult _NewArrivalProduct()
+        {
+            return PartialView();
+        }
+
+        // Top Categories
+        public ActionResult _TopCategories()
+        {
+            return PartialView();
+        }
+
+        // Popular Brands
+        public ActionResult _PopularBrands()
+        {
+            return PartialView();
+        }
+
+        // Random - Best Seller - Hot Sale
+        public ActionResult _Random_BestSeller_HotSale()
+        {
+            return PartialView();
+        }
+
+        // Clients Say
+        public ActionResult _ClientsSay()
+        {
+            return PartialView();
+        }
+
+        // Hot Deals
+        public ActionResult _HotDeals()
+        {
+            return PartialView();
+        }
+
+        // Special Offer
+        public ActionResult _SpecialOffer()
+        {
+            return PartialView();
+        }
+
+        // Main Slider
+        public ActionResult _MainSlider()
+        {
+            return PartialView();
+        }
+
+        // Latest News
+        public ActionResult _LatestNews()
+        {
+            return PartialView();
+        }
     }
 }
